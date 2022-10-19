5b376248127ce0dca8b6202a6b1a2ace9fd08773
vishwanathreddy9 <vyasa@asu.edu>
2022-10-19 12:39:40 -0700

    implemented login functionality

--- src/Facade.java
+++ src/Facade.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Facade {
 
-	private int UserType;
+	int UserType = -1;
 
 	private Product theSelectedProduct;
 
@@ -10,8 +12,34 @@ public class Facade {
 
 	private Person thePerson;
 
+	Scanner scan;
+
+	Login login;
+
 	public boolean login() {
+		scan = new Scanner(System.in);
+		System.out.println("");
+		System.out.println("------------------- [[ Welcome ]] --------------------");
+		boolean validateLogin = false;
+		while (!validateLogin) {
+			while (UserType != 1 && UserType != 0) {
+				System.out.println("Please select one of the following:");
+				System.out.println("[0.] Login as buyer ");
+				System.out.println("[1.] Login as seller");
+				UserType = scan.nextInt();
+				if (UserType != 1 && UserType != 0) {
+					System.out.println("Please make a valid selection");
+				}
+			}
+			System.out.println("Please enter your Username");
+			String username = scan.next();
+			System.out.println("Please enter your Password");
+			String password = scan.next();
+			login = new Login(UserType, username, password);
+			validateLogin = login.validateLogin();
+		}
 		return false;
+		//createUser(UserType);
 	}
 
 	public void addTrading() {
